26f561e274ae0dc803652bba25b0ea05c46cbb81
Olarn U <olarn.u@gmail.com>
2016-12-02 12:18:55 +0700

    Add DispatchGroup

--- TryPromise/ViewController.swift
+++ TryPromise/ViewController.swift
@@ -31,37 +31,80 @@ class ViewController: UIViewController {
     
     let url = "https://jsonplaceholder.typicode.com/posts/"
 
-    @IBAction func touchInsideRefreshButton(_ sender: Any) {
-        
+    @IBAction func touchInsideRefreshButton(_ sender: Any) {}
+
+    @IBAction func touchUpInsideOnTryPromiseKitButton(_ sender: Any) {
         _ = firstly {
-                self.getData(url: "\(self.url)1")
-    
+            self.getData(url: "\(self.url)1")
+
             }.then { dict -> Void in
                 self.lblStatus.text = "Status: callback from 1"
                 print(dict)
-            
+
             }.then { dict -> Promise<NSDictionary> in
                 self.getData(url: "\(self.url)2")
-            
+
             }.then { dict -> Void in
                 self.lblStatus.text = "\(self.lblStatus.text!), 2"
                 print(dict)
-            
+
             }.then { dict -> Promise<NSDictionary> in
                 self.getData(url: "\(self.url)3")
-            
+
             }.then { dict -> Void in
                 self.lblStatus.text = "\(self.lblStatus.text!), 3"
                 print(dict)
-            
+
             }.always {
                 print("done")
+        }
+    }
+
+    let dispatchGroup = DispatchGroup()
+
+    @IBAction func touchUpInsideOnTryDispatchGroupButton(_ sender: Any) {
+
+        print("Start!")
+
+        for i in 1...5 {
+            self.dispatchGroup.enter()
+            self.getData(
+                url: "\(self.url)\(i)",
+                success: { data in
+                    if let id = data["id"] {
+                        print("Returned ID => \(id)")
+                    } else {
+                        print("Returned ID is nil")
+                    }
+                    self.dispatchGroup.leave()
+            })
+        }
+
+        self.dispatchGroup.notify(queue: DispatchQueue.main) {
+            print("done!")
         }
     }
 }
 
 extension ViewController {
- 
+
+    func getData(
+        url: String,
+        success: @escaping (_ data: [String:Any]) -> ())
+    {
+        Alamofire
+            .request(url)
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let data):
+                    success(data as! [String:Any])
+                case .failure(let error):
+                    print(error)
+                }
+        }
+    }
+
     func getData(url: String) -> Promise<NSDictionary> {
         return Promise { fulfill, reject in
             Alamofire
